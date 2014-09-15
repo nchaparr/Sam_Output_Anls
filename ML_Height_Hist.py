01fba8910684450bbebac1627648f12a7291a3c6
Niamh Chaparro <niamh.chaparro@yhahoo.com>
2014-09-15 10:42:02 -0700

    Scaled_ML_Height_Hists by ML_Height_Hist

--- ML_Height_Hist.py
+++ ML_Height_Hist.py
@@ -13,6 +13,7 @@ from matplotlib.patches import Patch
 import sys
 sys.path.insert(0, '/tera/phil/nchaparr/python')
 import nchap_fun as nc
+from nchap_class import *
 from Make_Timelist import *
 import warnings
 warnings.simplefilter('ignore', np.RankWarning)
@@ -28,47 +29,51 @@ warnings.simplefilter('ignore', np.RankWarning)
 
 dump_time_list, time_hrs = Make_Timelists(1, 3600, 28800)
 
-date = "Mar52014"
+date = "Nov302013"
 
-rinovals = np.genfromtxt("/tera/phil/nchaparr/python/Plotting/"+date+"/data/invrinos")
+#rinovals = np.genfromtxt("/tera/phil/nchaparr/python/Plotting/"+date+"/data/invrinos")
 
 ml_height_hist_vars=[]
-
+points = For_Plots(date)
+rinovals = points.rinovals()
+AvProfVars = points.AvProfVars()
+         
 for i in range(len(dump_time_list)):
      dump_time=dump_time_list[i]
      if i == 4:
           ML_Heights=[]
           case_range = 10
           for case in range(case_range):
-               height = np.genfromtxt("/tera/phil/nchaparr/python/Plotting/"+date+"/data/heights0000028800")
-               ML_Heights.append(np.genfromtxt("/tera/phil/nchaparr/python/Plotting/"+date+"/data/mixed_layer_height_"+ str(case+1) + "_" + dump_time))
+               height = np.genfromtxt("/newtera/tera/phil/nchaparr/python/Plotting/"+date+"/data/heights0000028800")
+               ML_Heights.append(np.genfromtxt("/newtera/tera/phil/nchaparr/python/Plotting/"+date+"/data/mixed_layer_height_"+ str(case+1) + "_" + dump_time))
 
           ML_Heights=np.array(ML_Heights)
           [zvals, yvals, xvals] = ML_Heights.shape
           ML_Heights = np.reshape(ML_Heights, (zvals*yvals*xvals,))
           v_max, v_min, mean, var = np.amax(ML_Heights), np.amin(ML_Heights), np.mean(ML_Heights), np.var(ML_Heights)
-          print 'max min std', v_max, v_min, mean, var
-          rinovals_index = (i+1)*6-1
-          print rinovals_index
-          ml_height_hist_vars.append([rinovals[rinovals_index,1], rinovals[rinovals_index,3], v_max, v_min, mean, var])
+          #print 'max min std', v_max, v_min, mean, var
+          rinovals_index = (i+1)*4-1
+          #print rinovals_index
+          #ml_height_hist_vars.append([rinovals[rinovals_index,1], rinovals[rinovals_index,3], v_max, v_min, mean, var])
           #n, bins, patches = theAx.hist(tracer_peaks, bins=20)
           ML_Heights = np.reshape(ML_Heights, (zvals*yvals, xvals))
           height_bin_vols = nc.Bin_Peaks(ML_Heights, height)
-          #set up plot
 
+          #set up plot
+          h = AvProfVars[rinovals_index, 1]
           theFig = plt.figure()
           theFig.clf()
           theAx = theFig.add_subplot(111)
           #theAx.set_title('Histogram of local Mixed Layer Heights from 1 Case at 5 hrs')
-          theAx.set_xlabel('z (m)')
-          theAx.set_ylabel('count')
-          theAx.bar(height, height_bin_vols)     
-          theAx.set_xlim(0, 1500) #TODO:need to test axis limits first
-          theAx.set_ylim(0, 60000)
+          theAx.set_ylabel(r'$P \left(\frac{h^{l}_{0}}{h}\right)$', fontsize=15)
+          theAx.set_xlabel(r'$h^{l}_{0} / h$', fontsize=15)
+          theAx.bar(1.0*height/h, 1.0*height_bin_vols/(zvals*yvals*xvals), width = .001)     
+          theAx.set_xlim(0.6, 1.2) #TODO:need to test axis limits first
+          theAx.set_ylim(0, .1)
           plt.show()
-          theFig.savefig("/tera/phil/nchaparr/python/Plotting/"+date+"/pngs/ML_Height_hist.png")
+          theFig.savefig("/newtera/tera/phil/nchaparr/python/Plotting/"+date+"/pngs/Scaled_ML_Height_hist.png")
 
-np.savetxt("/tera/phil/nchaparr/python/Plotting/"+date+"/data/ml_height_hist_vars", np.array(ml_height_hist_vars), delimiter=' ')
+#np.savetxt("/tera/phil/nchaparr/python/Plotting/"+date+"/data/ml_height_hist_vars", np.array(ml_height_hist_vars), delimiter=' ')
 
     
     
