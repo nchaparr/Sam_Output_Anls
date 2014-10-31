58b5428d6ca5ed5ebe8d9e2c8fe7d1215caa7d56
Niamh Chaparro <niamh.chaparro@yhahoo.com>
2014-10-31 11:54:30 -0700

    Histogram plots look good

--- ML_Height_Hist.py
+++ ML_Height_Hist.py
@@ -28,9 +28,9 @@ warnings.simplefilter('ignore', np.RankWarning)
 
 
 dump_time_list, time_hrs = Make_Timelists(1, 3600, 28800)
-date_list = ["Mar52014", "Dec142013", "Mar12014"] # 
-width_list = [10, 10, 10] #.005, .005,
-alpha_list = [1, .5, .25] # 
+date_list = ["Mar52014", "Dec142013", "Mar12014"] #  
+width_list = [10, 10, 10] #  .005, .005, .005 
+alpha_list = [1, .5, .25] #  
 label_list=[r"$\overline{w^{'}\theta^{'}}_{s} = 150 \ Wm^{-2}$", r"$\overline{w^{'}\theta^{'}}_{s} = 100 \ Wm^{-2}$", r"$\overline{w^{'}\theta^{'}}_{s} = 60 \ Wm^{-2}$"] #
 color_list = ['k', '.5', '0'] # 
 theFig = plt.figure()
@@ -40,17 +40,22 @@ theAx = theFig.add_subplot(111)
 theAx.set_title(r"$\gamma = 10 \ (Kkm^{-1})$", fontsize=30)
 #theAx.set_ylabel(r'$P( \frac{h^{l}_{0}}{h} )$', fontsize=30)
 theAx.set_ylabel(r'$Count$', fontsize=30)
-theAx.set_xlabel(r'$h^{l}_{0}$', fontsize=30)
-theAx.set_xlim(400, 1400) #TODO:need to test axis limts first
+#theAx.set_xlabel(r'$\frac{h^{l}_{0}}{h}$', fontsize=32)
+theAx.set_xlabel(r'$h^{l}_{0}$', fontsize=32)
+#theAx.set_xticks([.6, .8, 1, 1.2])
+#theAx.set_yticks([0, 0.05, 0.1])
+theAx.set_xticks([500, 1000, 1500])
+theAx.set_yticks([0, 10000, 20000])
+theAx.set_xlim(400, 2100) #TODO:need to test axis limts first
 theAx.set_ylim(0, 25000)
 
-#theAx.set_xlim(0.4, 1.2) #TODO:need to test axis limts first
+#theAx.set_xlim(0.6, 1.2) #TODO:need to test axis limts first
 #theAx.set_ylim(0, .1)
 
 #theAx.text(1060, 10000, r"$\overline{w^{'}\theta^{'}}_{s} = 150 \ Wm^{-2}$", fontsize=20)
 #theAx.text(900, 12000, r"$\overline{w^{'}\theta^{'}}_{s} = 100 \ Wm^{-2}$", fontsize=20)
 #theAx.text(0.9, .04, r"$\overline{w^{'}\theta^{'}}_{s} = 60 \ Wm^{-2}$", fontsize=20)
-theAx.tick_params(axis='both', which='major', labelsize=20)
+theAx.tick_params(axis='both', which='major', labelsize=30)
 #rinovals = np.genfromtxt("/tera/phil/nchaparr/python/Plotting/"+date+"/data/invrinos")
 for j in range(len(date_list)):
     date=date_list[j]
@@ -90,8 +95,8 @@ for j in range(len(date_list)):
           #set up plot
             h = AvProfVars[rinovals_index, 1]
           #theAx.set_title('Histogram of local Mixed Layer Heights from 1 Case at 5 hrs')
-            theAx.bar(1.0*height, 1.0*height_bin_vols, width = Width, alpha = Alpha, color=Color, label=Label)     
-theAx.legend(loc = 'upper right', prop={'size':20})
+            theAx.bar(1.0*height, 1.0*height_bin_vols, width = Width, alpha = Alpha, color=Color, label=Label)#h  //(zvals*yvals*xvals)     
+theAx.legend(loc = 'upper right', prop={'size':28})
 theFig.tight_layout()          
 theFig.show()          
 #theFig.savefig("/newtera/tera/phil/nchaparr/python/Plotting/"+date+"/pngs/ML_Height_hist.png")
