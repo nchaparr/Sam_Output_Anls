4388b7928df92ee4b79125304925d073690c011a
Niamh Chaparro <niamh.chaparro@yhahoo.com>
2015-03-30 15:15:42 -0700

    redid the theta_grad and scaled_theta_grad profiles without legen box for submission

--- plot_theta_profs.py
+++ plot_theta_profs.py
@@ -26,12 +26,12 @@ Ax = Fig1.add_subplot(111)
 #Ax1.set_title( r'$\frac{\partial \theta}{\partial z}$', fontsize=20)
 #Ax1.set_xlabel(r"$\frac{\frac{\partial \theta}{\partial z}}{\gamma}$", fontsize=20)
 #Ax.set_xlabel(r"$\frac{ \partial \overline{\theta} }{\partial z} / \gamma$", fontsize=25)
-Ax.set_xlabel(r"$\frac{\partial \overline{\theta}}{\partial z}$ Km$^{-1}$", fontsize=25)
+Ax.set_xlabel(r"$\frac{\partial \overline{\theta}}{\partial z}/\gamma$ Km$^{-1}$", fontsize=25)
 #Ax.set_xlabel(r"$\overline{w^{,}\theta^{,}}$", fontsize=20)
 #Ax1.set_ylabel(r"$\frac{z}{h}$", fontsize=20)
 Ax.set_ylabel(r"$\frac{z}{h}$", fontsize=30)
 #plt.xlim(0, 1)
-plt.xlim(-.001, .02)
+plt.xlim(-.08, 2)
 #plt.ylim(50, 950)
 plt.ylim(0.2, 1.4)
 
@@ -44,7 +44,7 @@ flux_list = [100, 100, 60, 60, 150, 60, 150]
 gamma_list=[.005, .01, .005, .0025, .005, .01, .01]
 #choose a dumptime
 #dump_time, Time = dump_time_list[time_index], Times[time_index]
-#print Time
+##print Time
 dump_time = "0000010800"
 dump_time_index=29
 dump_time_index0=19
@@ -73,13 +73,13 @@ for i in range(len(theta_file_list)):
     dtheta = np.diff(theta)      
     dthetadz = np.divide(dtheta, dheight)        
     element0 = np.array([0])
-    dthetadz=np.hstack((element0, 1.0*dthetadz)) #*1.0/gamma
+    dthetadz=np.hstack((element0, 1.0*dthetadz/gamma)) #*1.0
         
     #only need up to 2500meters
     top_index = np.where(abs(1670 - height) < 40.)[0][0]
 
     #where gradient is max, and flux is min
-    #print AvProfVars[:,1].shape, height.shape
+    ##print AvProfVars[:,1].shape, height.shape
     
     if run_name == "Nov302013":
         h1 = AvProfVars[dump_time_index0, 1]
@@ -90,18 +90,18 @@ for i in range(len(theta_file_list)):
     h=height[h_index]    
     scaled_height = [1.0*ht/h for ht in height]
     
-    print h1, h_index, height[h_index]
+    #print h1, h_index, height[h_index]
     
     fluxes = np.multiply(wvelthetapert, rhow)*1004.0/flux_list[i]
 
     Ax.plot(dthetadz, scaled_height, marker_list[i], label = legend_list[i], markersize=10) #, 
 zeros = np.zeros_like(height)
-Ax.plot(zeros+.0002, scaled_height, 'k-')
-Ax.plot(zeros+.01, scaled_height, 'k-')
-Ax.plot(zeros+.005, scaled_height, 'k-')
-Ax.plot(zeros+.0025, scaled_height, 'k-')
-Ax.legend(numpoints=1, loc = 'lower right', prop={'size':14})
-Ax.set_xticks([.0002, .0025, .005, .01])
+Ax.plot(zeros+1, scaled_height, 'k-')
+Ax.plot(zeros+.03, scaled_height, 'k-')
+#Ax.plot(zeros+.005, scaled_height, 'k-')
+#Ax.plot(zeros+.0025, scaled_height, 'k-')
+#Ax.legend(numpoints=1, loc = 'lower right', prop={'size':14})
+Ax.set_xticks([.03, 1])
 Ax.tick_params(axis="both", labelsize=15)
 plt.tight_layout()
 plt.show()
