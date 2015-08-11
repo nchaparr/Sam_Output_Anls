b97794dca1a32df3aed24920819c5d8744f37412
Niamh Chaparro <niamh.chaparro@yhahoo.com>
2015-08-11 12:41:55 -0700

    theta_flux prof for mar1

--- plot_dthetaflux.py
+++ plot_dthetaflux.py
@@ -15,9 +15,9 @@ rcParams.update({'font.size': 10})
 
 """
 
-date = "Jan152014_1"
-sfc_flx = 150
-gamma = .005
+date = "Mar12014"
+sfc_flx = 60
+gamma = .01
 
 Fig1 = plt.figure(1)
 Fig1.clf()
@@ -33,8 +33,8 @@ Ax.set_xlabel(r"$\overline{\theta}$", fontsize=20)
 Ax.set_ylabel(r"$\frac{z}{h}$", fontsize=20)
 #Ax.set_ylabel(r"$z$", fontsize=20)
 plt.xlim(300, 312)
-plt.ylim(100, 1600)
-#plt.ylim(0.1, 1.4)
+#plt.ylim(100, 1600)
+plt.ylim(0.1, 1.4)
 
 
 Ax1 = Fig1.add_subplot(132)
@@ -42,15 +42,15 @@ Ax1 = Fig1.add_subplot(132)
 #Ax1.set_title( r'$\frac{\partial \theta}{\partial z}$', fontsize=20)
 Ax1.set_xlabel(r"$\frac{\frac{\partial \theta}{\partial z}}{\gamma}$", fontsize=20)
 #Ax1.set_xlabel(r"$\frac{\partial \theta}{\partial z}$", fontsize=20)
-Ax1.set_ylabel(r"$\frac{z}{h}$", fontsize=20)
+#Ax1.set_ylabel(r"$\frac{z}{h}$", fontsize=20)
 #start, end = -.025, .025
 start, end = -1, 2.5
 Ax1.set_xticks([.02, 1])
 #Ax1.set_ylabel(r"$z$", fontsize=20)
 #plt.xlim(-.025, .025)
 #plt.xlim(-1, 2.5)
-plt.ylim(100, 1600)
-#plt.ylim(0.1, 1.4)
+#plt.ylim(100, 1600)
+plt.ylim(0.1, 1.4)
 
 Ax2 = Fig1.add_subplot(133)
 #Ax2.set_title(r"$\overline{w^{'} \theta^{'}}$", fontsize=20)
@@ -63,17 +63,17 @@ Ax2.set_xlabel(r"$\frac{\overline{w^{'}\theta^{'}}}{\overline{w^{'}\theta^{'}}_{
 
 #Ax2.set_ylabel(r"$z$", fontsize=20)
 #Ax2.set_ylabel(r"$\frac{z}{h}$", fontsize=20)
-plt.ylim(100, 1600)
+#plt.ylim(100, 1600)
 #plt.xlim(-.06, .14)
 #plt.xlim(-.4, 1.2)
-#plt.ylim(0.1, 1.4)
+plt.ylim(0.1, 1.4)
 dump_time_list, Times = Make_Timelists(1, 600, 28800)
  
 theta_file_list = ["/newtera/tera/phil/nchaparr/python/Plotting/"+date+"/data/theta_bar"+ dump_time for dump_time in dump_time_list]
 press_file_list = ["/newtera/tera/phil/nchaparr/python/Plotting/"+date+"/data/press"+ dump_time for dump_time in dump_time_list]
 flux_file_list = ["/newtera/tera/phil/nchaparr/python/Plotting/"+date+"/data/wvelthetapert"+ dump_time for dump_time in dump_time_list]
 height_file = "/newtera/tera/phil/nchaparr/python/Plotting/"+date+"/data/heights0000000600"
-#AvProfVars = np.genfromtxt("/tera/phil/nchaparr/python/Plotting/"+date+"/data/AvProfLims")
+AvProfVars = np.genfromtxt("/newtera/tera/phil/nchaparr/python/Plotting/"+date+"/data/AvProfLims")
 
 #loop over text files files
 for i in range(len(theta_file_list)):
@@ -97,23 +97,23 @@ for i in range(len(theta_file_list)):
 
     #where gradient is max, and flux is min
     #print AvProfVars[:,1].shape, height.shape
-    #scaled_height = [1.0*h/AvProfVars[i,1] for h in height]
+    scaled_height = [1.0*h/AvProfVars[i,1] for h in height]
 
     fluxes = np.multiply(wvelthetapert, rhow)*1004.0/sfc_flx
     
-    #if np.mod(i+1, 6) == 0:
-    if i > 14 and i < 21:
+    if np.mod(i+1, 6) == 0:
+    #if i > 14 and i < 21:
         
         fluxes[0] = np.nan
         zeros = np.zeros_like(height)
 
-        Ax.plot(theta, height, '-') #, label = str(Times[i])+'hrs'
+        Ax.plot(theta, scaled_height, '-') #, label = str(Times[i])+'hrs'
         
-        Ax1.plot(1.0*dthetadz/gamma, height, '-', label = str(Times[i])+'hrs')
+        Ax1.plot(1.0*dthetadz/gamma, scaled_height, '-', label = str(Times[i])+'hrs')
         
-        Ax2.plot(fluxes, height, '-', label = str(Times[i])+'hrs')    
+        Ax2.plot(fluxes, scaled_height, '-', label = str(Times[i])+'hrs')    
     
-array = np.genfromtxt('/tera/phil/nchaparr/python/Pert_Files/snd')
+array = np.genfromtxt('/newtera/tera/phil/nchaparr/python/Pert_Files/snd')
     
 height_0 = array[:, 0]
 theta_0 = array[:, 1]
