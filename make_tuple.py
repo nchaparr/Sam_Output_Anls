d53c60369fe004a898d4961109ab7efdfe0bd559
Phil Austin <paustin@eos.ubc.ca>
2016-07-22 12:05:48 -0700

    missing items

---
+++ make_tuple.py
@@ -0,0 +1,24 @@
+from collections import namedtuple
+
+def make_tuple(in_dict,tupname='values'):
+    """
+    make a named tuple from a dictionary
+
+    Parameters
+    ==========
+
+    in_dict: dictionary
+         Any python object with key/value pairs
+
+    tupname: string
+         optional name for the new namedtuple type
+
+    Returns
+    =======
+
+    the_tup: namedtuple
+          named tuple with keys as attributes
+    """
+    the_tup = namedtuple(tupname, in_dict.keys())
+    the_tup = the_tup(**in_dict)
+    return the_tup
