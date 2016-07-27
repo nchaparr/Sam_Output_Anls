f095bf0172989ed71119ac130bc72eab5fa77405
Niamh Chaparro <niamh.chaparro@yhahoo.com>
2016-07-26 20:36:46 -0700

    updated plot_downwarm to plot quadrants at a level vs scaled time

--- plot_downwarm_h.py
+++ plot_downwarm_h.py
@@ -40,11 +40,13 @@ def Main_Fun(rundate, gamma, flux_s):
      #Get heights, scaling parameters
      AvProfVars = files.AvProfVars()
      rinovals = files.rinovals()
+     gm_vars = files.gm_vars()
      #print AvProfVars.shape, rinovals.shape
      upwarm_h0 = []
      downwarm_h0=[]
      upcold_h0=[]
      downcold_h0=[]
+     scaled_times=[]
      #loop over text files files
      for i in range(len(flux_quads_file_list)):
          #print i, theta_file_list[i]
@@ -59,8 +61,9 @@ def Main_Fun(rundate, gamma, flux_s):
          press = np.genfromtxt(press_file_list[i])
          rhow = nc.calc_rhow(press, height, theta[0])
          h = AvProfVars[j, 1]
-         h0=AvProfVars[j, 0]
+         h0=AvProfVars[j, 3]
          h1 = AvProfVars[j, 2]
+         scaled_time=np.divide(gm_vars[j, 3], gm_vars[j, 0])
          deltah = h1-h
          deltatheta = gamma*deltah
          thetastar = rinovals[j, 9]
@@ -77,17 +80,17 @@ def Main_Fun(rundate, gamma, flux_s):
          upwarm_h0.append(1.0*upwarm/(flux_s1))# // (thetastar) / /(gamma*deltah)/(0.2*thetastar)
          downwarm_h0.append(1.0*downwarm/(flux_s1))
          upcold_h0.append(1.0*upcold/(flux_s1))
-         downcold_h0.append(1.0*upcold/(flux_s1))
-
+         downcold_h0.append(1.0*downcold/(flux_s1))
+         scaled_times.append(scaled_time)
      upwarm_h0 = np.array(upwarm_h0)
      upcold_h0 = np.array(upcold_h0)
      downcold_h0 = np.array(downcold_h0)
      downwarm_h0 = np.array(downwarm_h0)
 
      if rundate=="Jan152014_1":
-         Times, upwarm_h0, downwarm_h0, upcold_h0, downcold_h0 = Times[0:11], upwarm_h0[0:11], downwarm_h0[0:11], upcold_h0[0:11], downcold_h0[0:11] 
+         scaled_times, upwarm_h0, downwarm_h0, upcold_h0, downcold_h0 = scaled_times[0:11], upwarm_h0[0:11], downwarm_h0[0:11], upcold_h0[0:11], downcold_h0[0:11] 
      
-     return upwarm_h0, downwarm_h0, upcold_h0, downcold_h0, Times    
+     return upwarm_h0, downwarm_h0, upcold_h0, downcold_h0, scaled_times    
 
 run_list = [["Dec142013", .01, 100, '100/10', 'kv'], ["Nov302013", .005, 100, '100/5','ko'], ["Dec202013", .005, 60,'60/5','yo'], ["Dec252013", .0025, 60,'60/2.5','y*'], ["Jan152014_1", .005, 150, '150/5','ro'], ["Mar12014", .01, 60,'60/10','yv'], ["Mar52014", .01, 150,'150/10','rv']]
 
@@ -108,17 +111,17 @@ Ax3.tick_params(axis="both", labelsize=20)
 #Ax3.set_ylabel(r"$\overline{w^{\prime-}\theta^{\prime+}}_{h}$ (ms$^{-1}$K)", fontsize=30)
 
 #Ax3.set_ylim(-.14, 0)
-Ax3.set_ylim(-0.5, 1)
+Ax3.set_ylim(-0.5, 0.5)
 #Ax3.set_ylim(-.5, 0)
 #Ax3.set_xlim(2, 8.2)
 
 for run in run_list:
     #print run[0]
-    upwarm_h0, downwarm_h0, upcold_h0, downcold_h0, Times = Main_Fun(run[0], run[1], run[2])
-    Ax3.plot(Times, 1.0*upwarm_h0, run[4])
-    Ax3.plot(Times, 1.0*downwarm_h0, run[4])
-    #Ax3.plot(Times, 1.0*upcold_h0, 'b-')
-    #Ax3.plot(Times, 1.0*downcold_h0, 'b-')
+    upwarm_h0, downwarm_h0, upcold_h0, downcold_h0, scaled_times = Main_Fun(run[0], run[1], run[2])
+    #Ax3.plot(scaled_times, 1.0*upwarm_h0, run[4])
+    #Ax3.plot(scaled_times, 1.0*downwarm_h0, run[4])
+    Ax3.plot(scaled_times, 1.0*upcold_h0, run[4])
+    Ax3.plot(scaled_times, 1.0*downcold_h0, run[4])
 #Ax3.legend(bbox_to_anchor=(1.49, 1.03), prop={'size':20}, numpoints = 1)
 #box = Ax3.get_position()
 #Ax3.set_position([box.x0, box.y0, box.width*1.33, box.height])
