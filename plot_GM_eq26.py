8d4c45ccd0e56b923ef4687ee5c7ce0e7c8a8be9
Niamh Chaparro <niamh.chaparro@yhahoo.com>
2016-08-25 16:53:09 -0700

    plotted delta/z_enc vs L_0/z_enc

--- plot_GM_eq26.py
+++ plot_GM_eq26.py
@@ -28,10 +28,11 @@ Times0 = np.array(Times0)
 label_list = ['100/10', '100/5', '60/5', '60/2.5', '150/5', '60/10', '150/10']
 legend_list = ['kv', 'ko', 'yo', 'y*', 'ro', 'yv', 'rv']
 Run_Date_List = ["Dec142013", "Nov302013", "Dec202013", "Dec252013", "Jan152014_1", "Mar12014", "Mar52014"]
-keep_x = np.empty([7,48],dtype=np.float)
-keep_y = np.empty_like(keep_x)
+keep_x = np.zeros([7,48],dtype=np.float)
+keep_y = np.zeros_like(keep_x)
 
-for i in range(len(label_list)):
+for i in range(len(label_list)):    
+      
     if i<99:
         
         #pull variables from text files 
@@ -49,24 +50,34 @@ for i in range(len(label_list)):
         c_squared_delta = rinovals[:, 10]
         c_delta = c_squared_delta**.5
 
-        Xs = (z_enc/L0) 
+        Xs = (L0/z_enc) 
         Ys = delta/z_enc
         #Ys = points.Deltah_over_h(8, 1, 7)
 
         if Run_Date_List[i] == "Nov302013":
             #clean up
-            Ys[7:] = np.nan
-            Ys[13] = np.nan
-            Ys[15:17] = np.nan
-            Ys[24:26] = np.nan
+            #Xs=np.array(Xs)
+            #Ys=np.array(Ys)
+            #print(Xs, Xs.shape)
+            #print(Ys, Ys.shape)
+            #Ys[7:] = np.nan
+            #Ys[13] = np.nan
+            #Ys[15:17] = np.nan
+            #Ys[24:26] = np.nan
             #
             # make all arrays the same size
             #
-            Xs = np.resize(Xs,(48,))
-            Ys = np.resize(Ys,(48,))
-            Xs[32:] = np.nan
-            Ys[32:] = np.nan
-            Ys[0] = np.nan #neg value
+            Xs = np.hstack((Xs,np.empty(16,)))
+            Ys = np.hstack((Ys,np.empty(16,)))
+            Ys[31:48] = np.nan
+            Xs[31:48] = np.nan
+            #print(Xs, Xs.shape)
+            #print(Ys, Ys.shape)
+            #print(keep_x[i,:], keep_x[i,:].shape)
+            #print(keep_y[i,:])
+            #Xs[32:] = np.nan
+            #Ys[32:] = np.nan
+            #Ys[0] = np.nan #neg value
             #print('Nov 30 deltas: ',Ys)
             #plot 
         elif Run_Date_List[i] == "Jan152014_1":
@@ -82,9 +93,10 @@ for i in range(len(label_list)):
             Ys[29:] = np.nan
         else:
             Ys[:11] = np.nan
-        keep_x[i,:] = Xs[:]
-        keep_y[i,:] = Ys[:]
-        print(Run_Date_List[i])
+        
+        keep_x[i,0:48] = Xs[0:48]
+        keep_y[i,0:48] = Ys[0:48]
+        #print(Run_Date_List[i])
 
 
 plt.close('all')
@@ -92,18 +104,26 @@ plt.close('all')
 fig1,ax1 = plt.subplots(1,1)
         
 for count,name in enumerate(Run_Date_List):
-    if name=="Nov302012":
-        ax1.plot(keep_x[count,:],keep_y[count,:], legend_list[count], label = label_list[count], markersize=10)
-        
-xs = np.arange(6, 40, 1)
-ys= (1)*xs**(-0.8)
-ax1.plot(xs, ys, 'k--')
+    #if name=="Nov302013":
+        #print(keep_x[count,:])
+        #print(count)
+        #print(keep_y[count,:])
+        #print(keep_x[count,:])
+    ax1.loglog(keep_x[count,:],keep_y[count,:], legend_list[count], label = label_list[count], markersize=10)
 
-ax1.text(6.6, .2, r'$y = \sqrt{0.4}x^{-\frac{2}{3}}$',  fontdict=None, withdash=False, fontsize = 25, rotation=-8)
-ax1.set_xlabel(r"$z_{enc}/L_{0}$", fontsize=20)
+xs = np.arange(0, .2, .00001)
+ys= (1)*xs**(.78)
+ax1.loglog(xs, ys, 'k--')
+
+ax1.text(.08, .15, r'$y = x^{.78}$',  fontdict=None, withdash=False, fontsize = 25, rotation=32)
+ax1.set_xlabel(r"$(z_{enc}/L_{0})^{-1}$", fontsize=20)
 ax1.set_ylabel(r"$\delta/z_{enc}$", fontsize=20)
-#plt.ylim(0, 0.2)
-plt.xlim(10,35)
+plt.ylim(0.03, 0.2)
+ax1.set_yticks([.1, .2])
+ax1.set_yticklabels([0.1, .2])
+plt.xlim(.03,.2)
+ax1.set_xticks([.1, .2])
+ax1.set_xticklabels([0.1, .2])
 ax1.tick_params(axis="both", labelsize=20)
 plt.tight_layout()
 
@@ -113,24 +133,23 @@ fig2,ax2 = plt.subplots(1,1)
 x_keep=[]
 y_keep=[]
 for count,name in enumerate(Run_Date_List):
-    good_points= np.logical_not(np.isnan(keep_y[count,:]))
-    good_x = keep_x[count,good_points]
-    good_y = keep_y[count,good_points]
-    print(good_y)
+    #good_points= np.logical_not(np.isnan(keep_y[count,:]))
+    #good_points=keep_x[count,:]
+    good_x = keep_x[count,0:48]#[count,good_points]
+    good_y = keep_y[count,0:48]#[count,good_points]
     hit = good_y < 0
-    #if np.sum(hit) > 0:
-        #print("found neg delta for {}".format(name))
+    if np.sum(hit) > 0:
+        print("found neg delta for {}".format(name))
     x=np.log10(good_x)
-    y=np.log10(good_y)
-    print(y)
+    y=np.log10(good_y)   
     ax2.plot(x,y, legend_list[count], label = label_list[count], markersize=10)
-    x_keep.extend(good_x)
-    y_keep.extend(good_y)
+    x_keep.extend(good_x[0:48])
+    y_keep.extend(good_y[0:48])
 
-    
+print(len(x_keep), len(y_keep))    
 x_keep=np.array(x_keep)
 y_keep=np.array(y_keep)
-
+print(x_keep.shape, y_keep.shape)
 fig2,ax2 = plt.subplots(1,1)
 
 x=np.log10(x_keep)
@@ -139,7 +158,10 @@ ax2.plot(x,y, legend_list[count], label = label_list[count], markersize=10)
 
 from scipy.stats import linregress
 
-results = linregress(np.log10(x_keep), np.log10(y_keep))
+logx=np.log10(x_keep)
+logy=np.log10(y_keep)
+mask=~np.isnan(logx)&~np.isnan(logy)
+results = linregress(logx[mask], logy[mask])
 xs = np.log10(np.arange(6, 40, 1))
 ys=  results.intercept + results.slope*xs
 ax2.plot(xs, ys, 'k--')
