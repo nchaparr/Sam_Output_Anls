aa6a70d008b00bf1d14113414a3d19acb64c3de2
Phil Austin <paustin@eos.ubc.ca>
2016-12-22 11:23:10 -0800

    add png file

--- find_heights.py
+++ find_heights.py
@@ -1,8 +1,11 @@
 import h5py
 from enum import IntEnum
 from collections import OrderedDict as od
+from collections import defaultdict
 import numpy as np
 from matplotlib import pyplot as plt
+plt.style.use('ggplot')
+
 
 #https://github.com/nchaparr/Sam_Output_Anls/blob/master/Readme.txt#L45    
 class Heights(IntEnum):
@@ -15,16 +18,18 @@ class Heights(IntEnum):
     deltheta = 6
     mltheta=7
     invrinos=8
-
 #
 # get all 6 heigths
 #
 columns=list(Heights.__members__.keys())[:6]
-
+var_dict=defaultdict(list)
 h5_profiles='profiles_flux_quads_260.h5'
 with h5py.File(h5_profiles,'r') as infile:
     case_list=list(infile.keys())[:1]
     case=case_list[0]
+    avg_theta = infile[case]['ens_av_thetas'][...]
+    avg_theta=np.mean(avg_theta,axis=(1,2))
+    print('shape: ',avg_theta.shape)
     height = infile[case]['height'][...]
     hvals = infile[case]['hvals'][...]
     print('case: ',case)
@@ -53,14 +58,19 @@ with h5py.File(prof_file,'r') as flux_in:
         hit = np.logical_and(thetapert >0, wpert >0)
         theta_uw=thetapert[hit]
         theta_out = np.sqrt(np.mean(theta_uw**2.))
+        theta_std = np.std(theta_uw)
+        theta_count = len(theta_uw)
         flux=(wpert*thetapert).mean()
-        mean_flux.append(flux)
-        theta_rms.append(theta_out)
+        var_dict['mean_flux'].append(flux)
+        var_dict['theta_rms'].append(theta_out)
+        var_dict['theta_std'].append(theta_std)
+        var_dict['theta_count'].append(theta_count)
         
 plt.close()
 
-def plot2(prof1,prof2,zlevs,height_dict):
-    fig,ax1=plt.subplots(1,1)
+def plot2(ax1,var_dict,key1,key2,zlevs,height_dict):
+    prof1=var_dict[key1]
+    prof2=var_dict[key2]
     ax2 = ax1.twiny()
     ax1.plot(prof1,zlevs,'b-')
     ax2.plot(prof2,zlevs,'r-')
@@ -70,16 +80,31 @@ def plot2(prof1,prof2,zlevs,height_dict):
         t2.set_color('r')
     for key,hlev in height_dict.items():
         ax1.text(0.,hlev,key)
+    ax1.set_xlabel('net flux (K m/s)')
+    ax1.xaxis.label.set_color('b')
+    ax2.xaxis.label.set_color('r')
     return ax1,ax2
 
+
 zlevs=height[levs]
-ax1,ax2 = plot2(mean_flux,theta_rms,zlevs,height_dict)
-ax1.set_xlabel('net flux (K m/s)')
-ax1.xaxis.label.set_color('b')
+avg_theta=avg_theta[levs]
+var_dict['avg_theta']=avg_theta
+fig,axes=plt.subplots(2,2,figsize=(14,14))
+ax1,ax2 = plot2(axes[0,0],var_dict,'mean_flux','theta_rms',zlevs,height_dict)
 ax2.set_xlabel('thetarms (K)')
 ax2.xaxis.label.set_color('r')
 
+ax1,ax2 = plot2(axes[0,1],var_dict,'mean_flux','theta_std',zlevs,height_dict)
+ax2.set_xlabel('thetstd (K)')
+
+ax1,ax2 = plot2(axes[1,0],var_dict,'mean_flux','theta_count',zlevs,height_dict)
+ax2.set_xlabel('thetacount')
+
+ax1,ax2 = plot2(axes[1,1],var_dict,'mean_flux','avg_theta',zlevs,height_dict)
+ax2.set_xlabel('avg_theta')
 
 
+fig.subplots_adjust(hspace=0.5)
+fig.savefig('fourplot.png')
         
 plt.show()
